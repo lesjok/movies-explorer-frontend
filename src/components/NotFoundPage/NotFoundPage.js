5c833e752b501580060926f5a2175117b9ad9942
lesjok <lesjok@inbox.ru>
2022-11-21 23:59:26 +0300

    правка ошибок ревью 1

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -9,7 +9,7 @@ function NotFoundPage() {
       <div className="not-found__container">
         <h1 className="not-found__title">404</h1>
         <p className="not-found__description">Страница не найдена</p>
-        <button onClick={history.goBack()} className="not-found__link">Назад</button>
+        <button onClick={history.goBack} className="not-found__link">Назад</button>
       </div>    
     </section>
   );
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,8 +5,9 @@ import { useFormWithValidation } from "../../hooks/useFormWithValidation.js";
 import React from "react";
 function SearchForm({ onSearch, filterShortMovies, isActiveCheckbox, searchWord  = ''}) {
 const {values, handleChange, errors, isValid, setValues } = useFormWithValidation();
-  const [clicked, setClicked] = React.useState(false);
-  const [textError] = React.useState('Нужно ввести ключевое слово');
+  // const [clicked, setClicked] = React.useState(false);
+  // const [textError] = React.useState('Нужно ввести ключевое слово');
+  const [error, setError] = React.useState('');
 
   useEffect(() => {
     setValues({
@@ -14,21 +15,26 @@ const {values, handleChange, errors, isValid, setValues } = useFormWithValidatio
   });
   }, [setValues, searchWord]);
 
-  const clickHandler = () => {
-    setClicked(true);
+  const changeHandler = () => {
+    // setClicked(true);
+  if (searchWord.length !== 0) setError('');
   }
 
   const searchMovies = (e) => {
     e.preventDefault();
     onSearch(values.search);
+    if (values.search.length === 0) {
+    setError('Нужно ввести ключевое слово');
+    return;
+  }
   }
 
   return (
     <form className="search-form" name="search" onSubmit={searchMovies}>
       <div className="search-form__container">
-        {(!isValid && clicked) && <span className="input__error search__error">{textError}</span>}
-        <input className="search-form__input" placeholder="Фильм" type="text" name="search" value={values.search} onChange={handleChange} required/>
-        <button className="search-form__btn form__btn" type="submit" onClick={clickHandler} disabled={!isValid && clicked}>Поиск</button>
+        {values.search.length === 0 && <span className="input__error search__error">{error}</span>}
+        <input className="search-form__input" placeholder="Фильм" type="text" name="search" value={values.search} onChange={changeHandler}/>
+        <button className="search-form__btn form__btn" type="submit" disabled={values.search.length === 0}>Поиск</button>
         <FilterCheckbox filterShortMovies={filterShortMovies} isActiveCheckbox={isActiveCheckbox} />
         <hr className="search-form__hr"></hr>
       </div>         
