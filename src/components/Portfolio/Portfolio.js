05e6510c1a3b430cf38406908f6677e0e7b870c3
lesjok <lesjok@inbox.ru>
2022-12-09 13:30:52 +0300

    change my portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,7 +8,7 @@ function Portfolio() {
       <nav>
         <ul className="portfolio__list list">
           <li className="list__item item">
-            <a href="https://lesjok.github.io/russian-travel/" className="item__link" target="blank">
+            <a href="https://visionary-panda-dc9de0.netlify.app/" className="item__link" target="blank">
               <p className="item__name">Статичный сайт</p>
               <img src={arrow} className="item__arrow" alt="arrow" />
             </a>         
@@ -20,7 +20,7 @@ function Portfolio() {
             </a>       
           </li>
           <li className="list__item item">
-            <a href="https://lesjok.github.io/russian-travel/" className="item__link" target="blank">
+            <a href="https://lesjok.github.io/HR-landing/" className="item__link" target="blank">
               <p className="item__name">Одностраничное приложение</p>
               <img src={arrow} className="item__arrow" alt="arrow" /> 
             </a>    
